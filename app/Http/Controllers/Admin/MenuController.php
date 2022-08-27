2c64e1f214a3df7f52288e7434bb6e6a1211f521
MANH-KHUAT <manhkd@trithucmoi.co>
2022-08-27 23:18:14 +0700

    List menu

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -5,18 +5,38 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Menu\CreateFormRequest;
 use Illuminate\Http\Request;
+use App\Http\Services\Menu\MenuService;
 
 class MenuController extends Controller
 {
+    protected $menuService;
+
+    public function __construct(MenuService $menuService)
+    {
+
+        $this->menuService = $menuService;
+    }
+
     public function create()
     {
       return view('admin.menu.add', [
-          'title' => 'Thêm Danh Mục'
+          'title' => 'Thêm Danh Mục',
+          'menus' => $this->menuService->getParent()
       ]);
     }
 
     public function store(CreateFormRequest $request)
     {
+        $this->menuService->create($request);
 
+        return redirect()->back();
+    }
+
+    public function index()
+    {
+        return view('admin.menu.list', [
+            'title' => 'Danh Sách Danh Mục Mới Nhất',
+            'menus' => $this->menuService->getAll()
+        ]);
     }
 }
