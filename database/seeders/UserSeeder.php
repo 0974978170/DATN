2203390964f99452b14c27ff492f60c4f04a40e6
manhkd <manhkd@trithucmoi.co>
2022-11-29 12:04:22 +0700

    fix seed user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,6 +22,11 @@ class UserSeeder extends Seeder
                 'email' => 'admin@gmail.com',
                 'password' => Hash::make('password'),
                 'remember_token' => Str::random(10),
+                'last_name' => 'admin',
+                'first_name' => 'admin',
+                'gender' => 'Male',
+                'roles' => 1,
+                'phone_number' => '0328337535',
                 'created_at'  => date('Y-m-d H:i:s'),
                 'updated_at'  => date('Y-m-d H:i:s')
             ]
